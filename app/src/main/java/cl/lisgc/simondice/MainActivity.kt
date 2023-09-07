a342a8ee57f7ba779be48d71e58de7437e44d31a
LisGC <65579390+LisGC@users.noreply.github.com>
2023-09-07 10:50:38 -0300

    cambios pocos(intentos)

--- app/src/main/java/cl/lisgc/simondice/MainActivity.kt
+++ app/src/main/java/cl/lisgc/simondice/MainActivity.kt
@@ -7,5 +7,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+/*
+        val button = findViewById<Button>(R.id.aboutButton)
+                buttonIncrement.setOnClickListener {
+                    val intentAbout = Intent(pack)
+
+                }
+*/
     }
 }
