6bb1173329f700e53eb217e56cb48e8d9e5ea2c6
LisGC <65579390+LisGC@users.noreply.github.com>
2023-10-21 12:41:10 -0300

    CAMBIOS

--- app/src/main/java/cl/lisgc/simondice/MainActivity.kt
+++ app/src/main/java/cl/lisgc/simondice/MainActivity.kt
@@ -1,7 +1,10 @@
 package cl.lisgc.simondice
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,22 +12,26 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
     }
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.mainmenu, menu)
-        return true
+        menuInflater.inflate(R.menu.mainmenu,menu)
+        return super.onCreateOptionsMenu(menu)
     }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.button_settings -> {
+        when(item.itemId) {
+            R.id.button_about -> {
                 val intentPreferences = Intent(this, SettingsActivity::class.java)
                 startActivity(intentPreferences)
                 return true
             }
-            R.id.button_about -> {
+
+            R.id.button_settings -> {
                 val intentPreferences = Intent(this, AboutActivity::class.java)
                 startActivity(intentPreferences)
                 return true
             }
+
             else -> return super.onOptionsItemSelected(item)
         }
     }
--- app/src/main/java/cl/lisgc/simondice/SplashScreenActivity.kt
+++ app/src/main/java/cl/lisgc/simondice/SplashScreenActivity.kt
@@ -1,57 +1,30 @@
 package cl.lisgc.simondice
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 
 class SplashScreenActivity : AppCompatActivity() {
-    val tagLog = "SplashScreenActivity"
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
+        var max: Long = 2500
+        var x: Long = 500
 
-        // Getting preference
-        //val sharedPreferences = getSharedPreferences("MyPreferences", Context.MODE_PRIVATE)
-        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
-        // Read a preference
-        val value = sharedPreferences.getString("splash_time", "1000")
-
-        Log.i(tagLog,"Value of splash_time:" + value)
-
-        // Load the fade-in animation
-        val fadeIn = AnimationUtils.loadAnimation(applicationContext, R.anim.fadein)
-
-
-        val imageView = findViewById<ImageView>(R.id.imageLogo)
-
-        fadeIn.setAnimationListener(object : Animation.AnimationListener {
-            override fun onAnimationStart(animation: Animation?) {
-                // Animation started
+        val timer = object : CountDownTimer(max,x) {
+            override fun onTick(millisUntilFinished: Long) {
+                var x: Long = max - millisUntilFinished
             }
 
-            override fun onAnimationEnd(animation: Animation?) {
-                // Animation ended; proceed to MainActivity
-                val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            override fun onFinish() {
+                var intent = Intent(this@SplashScreenActivity, MainActivity::class.java)
                 startActivity(intent)
-                finish() // Optional: Close the splash screen activity
-            }
 
-            override fun onAnimationRepeat(animation: Animation?) {
-                // Animation repeated
-            }
-        })
-        imageView.startAnimation(fadeIn)
-        val seconds = value?.toLong()
-        val delayMillis = seconds!!.toLong() // 2 seconds (adjust as needed)
 
-///lo de hacer el fade y que termine
-        val handler = Handler(Looper.getMainLooper())
-        handler.postDelayed({
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-            finish() // Optional: Close the splash screen activity
-        }, delayMillis)
+            }
+        }
+        timer.start()
     }
-}
 }
