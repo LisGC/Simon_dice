9765337949940daf38141e78842475a75e80316a
LisGC <65579390+LisGC@users.noreply.github.com>
2023-10-26 09:52:53 -0300

    actividad touch terminada, se realizo el cambio de imagen y texto segun el gesto realizado

--- app/src/main/java/cl/lisgc/simondice/TouchActivity.kt
+++ app/src/main/java/cl/lisgc/simondice/TouchActivity.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.view.GestureDetector
 import android.view.MotionEvent
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.core.view.GestureDetectorCompat
 
 private const val DEBUG_TAG = "Gestures"
@@ -13,15 +14,16 @@ private const val DEBUG_TAG = "Gestures"
     class TouchActivity : AppCompatActivity() {
 
         private lateinit var ratImg : ImageView
+        private lateinit var touchText : TextView
         private lateinit var mDetector: GestureDetectorCompat
 
         public override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
             setContentView(R.layout.activity_touch)
-            mDetector = GestureDetectorCompat(this, MyGestureListener())
+            mDetector = GestureDetectorCompat(this, GestureListener())
 
             ratImg = findViewById(R.id.ratImage)
-            //ratImg.setImageResource(R.drawable.rata2)
+            touchText = findViewById(R.id.textTouch)
 
         }
 
@@ -31,15 +33,23 @@ private const val DEBUG_TAG = "Gestures"
         }
 
 
-        class MyGestureListener : GestureDetector.SimpleOnGestureListener() {
+        inner class GestureListener : GestureDetector.SimpleOnGestureListener() {
 
             override fun onDown(event: MotionEvent): Boolean {
-                Log.d(DEBUG_TAG, "onDown: $event")
+                ratImg.setImageResource(R.drawable.rata3)
+                touchText.setText(R.string.ontouch)
                 return true
             }
             override fun onFling( event1: MotionEvent,event2: MotionEvent,velocityX: Float,velocityY: Float): Boolean {
-                Log.d(DEBUG_TAG, "onFling: $event1 $event2")
+                ratImg.setImageResource(R.drawable.rata2)
+                touchText.setText(R.string.fling)
                 return true
+            }
+
+            override fun onLongPress(e: MotionEvent) {
+                super.onLongPress(e)
+                ratImg.setImageResource(R.drawable.rata1)
+                touchText.setText(R.string.press)
             }
         }
 
